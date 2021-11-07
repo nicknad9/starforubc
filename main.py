2708f5f966076a7865690cda4b36ae77d8ffb7ef
Micaela Peragallo <ryankaufmannprof@gmail.com>
2021-11-06 21:05:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,11 @@ def SaveFile(pressure, energy, density, temperature, X, Y, Z):
     df.to_csv('data.csv', mode='a', index=False, header=False)
 
 L = np.arange(-15, 15, 1) + 0.5
-X, Y, Z = np.meshgrid(L, L, L)
+X, Y = np.meshgrid(L, L)
 
-grav = 0.1 * np.array([X, Y, Z]) / np.power((X*X + Y*Y + Z*Z), 3)
-rho = np.exp(-(X*X + Y*Y + Z*Z) / 200)
+grav = 0.1 * np.array([X, Y]) / np.power((X*X + Y*Y), 3)
+rho = np.exp(-(X*X + Y*Y) / 200)
 vel = np.array([np.random.random((30, 30, 30)),
-                np.random.random((30, 30, 30)),
                 np.random.random((30, 30, 30))])
 internal = rho * 0.01
 energy = internal + 0.5 * rho * np.sum(vel * vel)
@@ -33,28 +32,24 @@ df = pd.DataFrame(columns=[])
 
 df['X'] = X.flatten()
 df['Y'] = Y.flatten()
-df['Z'] = Z.flatten()
 df['rho'] = rho.flatten()
 df['temp'] = (pressure / rho).flatten()
-df['pressure'] = pressure.flatten()
-df['E'] = energy.flatten()
 df.to_csv('data.csv', mode='a', index=False, header=False)
 
-# Velocity: 3D Vector with [Vx (LxLxL), Vy(LxLxL), Vz (LxLxL)]
-# Rho: 3-dimensional Array of Scalars
-# Grav: 3D Vector with [Gx (LxLxL), Gy (LxLxL), Gz (LxLxL)]
-# Energy: 3-dimensional Array of Scalars
-# Pressure: 3-dimensional Array of Scalars
+# Velocity: 2D Vector with [Vx (LxLxL), Vy(LxLxL), Vz (LxLxL)]
+# Rho: 2-dimensional Array of Scalars
+# Grav: 2D Vector with [Gx (LxLxL), Gy (LxLxL), Gz (LxLxL)]
+# Energy: 2-dimensional Array of Scalars
+# Pressure: 2-dimensional Array of Scalars
 
-grav_x, grav_y, grav_z = grav[0], grav[1], grav[2]
+grav_x, grav_y = grav[0], grav[1]
 padded_grav_x = np.pad(grav_x, ((1,1), (1,1), (1,1)), mode='constant')
 padded_grav_y = np.pad(grav_y, ((1,1), (1,1), (1,1)), mode='constant')
-padded_grav_z = np.pad(grav_z, ((1,1), (1,1), (1,1)), mode='constant')
-grav_pad = np.array([padded_grav_x, padded_grav_y, padded_grav_z])
+grav_pad = np.array([padded_grav_x, padded_grav_y])
 
 for step in range(0, numsteps):
     rho, vel, energy, pressure = MHD.Update(rho, vel, energy, pressure, grav_pad, timestep)
     temperature = pressure / rho
-    SaveFile(pressure, energy, rho, temperature, X, Y, Z)
+    SaveFile(pressure, energy, rho, temperature, X, Y)
 
 vs.visualize("data.csv", 27000, 30)
